9d893138851837a552664a6da062462292a415ce
farchr186 <2018371104@uteq.edu.mx>
2021-09-24 20:25:52 -0500

    se agregar proy

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        int contador;
+        string message;
+        Button button1;
+        public MainPage()
+        {
+            InitializeComponent();
+            contador = 0;
+        }
+
+        private void IniciaContador(object sender, EventArgs e)
+        {
+
+            var button = ((Button)sender);
+            if (button1 == null)
+            {
+                button1 = ((Button)sender);
+            }
+            contador++;
+            message = "Numero de clicks {0} veces";
+            button.Text = String.Format(message, contador);
+        }
+
+        private void ReiniciaContador(object sender, EventArgs e)
+        {
+
+            contador = 0;
+
+            if (button1 != null)
+            {
+                button1.Text = message;
+            }
+        }
+
+
+    }
+}
